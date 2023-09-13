bec6c299e72033d59a0cd58911bf53ab164ad740
mikamatiotti <106685206+mikamatiotti@users.noreply.github.com>
2023-09-13 08:50:06 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementAudio) {
+  document.querySelector(idElementAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+  const listaDeTeclas = document.querySelectorAll('.tecla');
+
+  //para
+  for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1]
+
+  const idAudio = '#som_${instrumento}';
+
+    
+   tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+   console.log(contador);
+
+  }
